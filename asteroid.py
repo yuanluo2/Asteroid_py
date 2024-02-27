da9812b0466669c02b0ae7ae9e1cd42e30d39721
Yuan <49439486+yuanluo2@users.noreply.github.com>
2024-02-27 08:59:37 +0800

    Update asteroid.py

--- asteroid.py
+++ asteroid.py
@@ -22,8 +22,6 @@ PLAYER_INIT_SPEED = 8
 COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0, 0, 0)
 COLOR_GREEN = (0, 255, 0)
-COLOR_YELLOW = (255, 255, 0)
-COLOR_BLUE = (0, 0, 255)
 
 # init.
 pygame.init()
